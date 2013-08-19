d4c625f19642e2a8e44f493c814c0a86e03cf01b
theburningmonk <theburningmonk@gmail.com>
2013-08-20 00:54:37 +0100

    - removed generated dart files

--- Whack_A_Meng/packages/Whack_A_Meng/src/LevelResult.dart
+++
@@ -1,6 +0,0 @@
-part of whack_a_meng;
-
-class LevelResult {
-  static int TimeOut = 0; // time ran out before player was able to reach target
-  static int Win     = 1; // player reached target before time ran out
-}
--- Whack_A_Meng/packages/stagexl/src/filters/BitmapFilter.dart
+++
@@ -1,8 +0,0 @@
-part of stagexl;
-
-abstract class BitmapFilter {
-  
-  BitmapFilter clone();
-  void apply(BitmapData sourceBitmapData, Rectangle sourceRect, BitmapData destinationBitmapData, Point destinationPoint);
-  Rectangle getBounds();
-}
